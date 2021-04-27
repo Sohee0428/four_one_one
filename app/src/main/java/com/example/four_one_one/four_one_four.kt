05883db6c35979e139d9b0365117aa0206b2dc83
Sohee <sohee3480@gmail.com>
2021-04-27 19:01:41 +0900

    const 이용 클래스, 함수 내부에는 const로 변수를 선언할 수 없습니다.

--- app/src/main/java/com/example/four_one_one/four_one_four.kt
+++ app/src/main/java/com/example/four_one_one/four_one_four.kt
@@ -14,3 +14,17 @@ fun main() {
     myBool = true
     print(myVal)
 }
+
+
+
+//const이용
+
+const val myConst: Int = 10
+const var myConst2: Int = 10
+
+class MyClass {
+    const val myConst3 = 30
+}
+fun some(){
+    const val myConst4 = 40
+}
