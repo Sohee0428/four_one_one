e2c64e9be11c16d5f3f844376d814a0779ea424d
Sohee <sohee3480@gmail.com>
2021-04-27 18:52:40 +0900

    null이 될 수 있는 변수와 null 타입에 ?를 이용하여 null값을 대입

---
+++ app/src/main/java/com/example/four_one_one/four_one_three.kt
@@ -0,0 +1,10 @@
+package com.example.four_one_one
+
+val nonNullData: String = null
+val nullableData1: String? = null
+var nullableData2: String? = null
+
+fun main(){
+    nonNullData = "hello"
+    nullableData2 = "hello"
+}
