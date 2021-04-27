16dd1a75e04202146dd2889c539f113b90bb7d4c
Sohee <sohee3480@gmail.com>
2021-04-27 18:46:28 +0900

    코드 에러 수정본

--- app/src/main/java/com/example/four_one_one/four_one_two.kt
+++ app/src/main/java/com/example/four_one_one/four_one_two.kt
@@ -2,22 +2,23 @@ package com.example.four_one_one
 
 
 // 최상위 레벨
-val topData1: Int
-var topData2: Int
+val topData1: Int = 10
+var topData2: Int = 20
 
 
 // 클래스의 멤버 변수
 class User {
-    val objData1: String
-    var objData2: String
+    val objData1: String = "hello"
+    var objData2: String = "world"
 
 
-//    함수 내부의 지역 변수
+    //    함수 내부의 지역 변수
     fun some() {
         val localData1: Int
         var localData2: String
 
-        println(lacalData1)
+        localData1 = 40
+        println(localData1)
 
         localData2 = "hello"
         println(localData2)
